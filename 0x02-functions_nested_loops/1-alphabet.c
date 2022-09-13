73b3959a00efa052cb13cbec926b8b97675c5e98
Nour Hossam Eldin Radwan <nourhradwan95@gmail.com>
2022-09-13 09:20:07 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - check the code for Coding School students.
+ *
+ * Description: Print lower case alphabet from a - z
+ * using a for loop.
+ * Return: Always 1.
+ */
+void print_alphabet(void)
+{
+int i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+
+
