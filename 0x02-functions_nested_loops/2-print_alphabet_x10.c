7d1ed39ed4777a6ef78b9f27d74ace345724ca09
Nour Hossam Eldin Radwan <nourhradwan95@gmail.com>
2022-09-13 09:34:55 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function with no arguments
+ *
+ * Description: prints 10 times the alphabet
+ *
+ */
+void print_alphabet_x10(void)
+{
+int i;
+for(i = 0; i <= 9; i++)
+print_alphabet();
+_putchar('\n');
+
+}
