305b908dd325ea923afb813ce8cc937714edd0ce
Nour Hossam Eldin Radwan <nourhradwan95@gmail.com>
2022-09-13 09:40:33 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,11 +7,10 @@
  */
 void print_alphabet_x10(void)
 {
-int i, j;
-
-for(i = 0; i <= 9; i++)	
+int i, j
+for (i = 0; i <= 9; i++)
 {
-for(j = 'a'; j <= 'z'; j++)
+for (j = 'a'; j <= 'z'; j++)
 {
 _putchar(j);
 }
