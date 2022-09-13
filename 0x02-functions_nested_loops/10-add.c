78981e81cee97cc0657dd0224f850c4dc7514473
NourHRadwan <106489456+NourHRadwan@users.noreply.github.com>
2022-09-13 23:55:09 +0300

    allfiles

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - to add two integers and return the result
+ * @num1: for operand number one
+ * @num2: for operand number two
+ *
+ * Description: result integer variable is sum of num1 and num2
+ * Return: result
+ */
+int add(int num1, int num2)
+{
+int result;
+result = num1 + num2;
+return (result);
+}
+
+
---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - with one argument
+ * @n: is the input for function
+ *
+ * Description: if/else to check if n is negative
+ * zero or positive
+ * Return: 1 if greater than 0, 0 if 0, -1 if lesser
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+return (-1);
+}
+else
+_putchar('0');
+return (0);
+}
