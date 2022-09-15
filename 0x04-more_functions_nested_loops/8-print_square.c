35d8f0b4cd8494d841764a0487abf9ec728685eb
Nour Hossam Eldin Radwan <nourhradwan95@gmail.com>
2022-09-15 13:35:25 -0700

    8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,27 +1,27 @@
-#include "main.h"
+#include "coding.h"
 
 /**
- * print_square - function with int type one argument
- * @size: 1st int argument
- *
- * Description: nested loops to pring square with new line
- * Return: na
- */
+   * print_square - function with int type one argument
+    * @size: 1st int argument
+     *
+      * Description: nested loops to pring square with new line
+       * Return: na
+        */
 void print_square(int size)
 {
-if (size <= 0)
-{
-	_putchar('\n');
+		int i, j;
+
+			if (size > 0)
+					{
+								for (i = 0; i < size; i++)
+											{
+															for (j = 0; j < size; j++)
+																			{
+																								_putchar(																			}
+_putchar('\n');}
+
 }
 else
 {
-	int i, j;
-	for (i = 0; i < size; i++)
-	{
-	for (j = 0; j < size; j++)
-	{
-	_putchar('#');
-}
-_putchar('\n');
-}
+_putchar('\n');}
 }
