8e90aa1b9f928e9aa452512d7a7b89e9aefa4f5d
NourHRadwan <106489456+NourHRadwan@users.noreply.github.com>
2022-09-19 21:23:34 +0300

    task0

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,23 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    int a;
-    int b;
-
-    a = 98;
-    b = 42;
-    printf("a=%d, b=%d\n", a, b);
-    swap_int(&a, &b);
-    printf("a=%d, b=%d\n", a, b);
-    return (0);
-}
 /**
  * reset_to_98 - function with one argument
  * @n: int type pointer n
