2fc71d07435db3b627c8e7454d5031861be6827e
Nour Hossam Eldin Radwan <nourhradwan95@gmail.com>
2022-10-01 00:39:54 -0700

    8

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,24 +4,24 @@ int findLength(char *s);
 int helper(char *s, int len, int a);
 
 /**
-   * is_palindrome - function with 1 argument
-    * @s: char type pointer argument
-     *
-      * Description: test if string is palindrome
-       * Return: 1 if palindrome or 0 if not
-        */
+ * is_palindrome - function with 1 argument
+ * @s: char type pointer argument
+ *
+ * Description: test if string is palindrome
+ * Return: 1 if palindrome or 0 if not
+ */
 int is_palindrome(char *s)
 {
 return (helper(s, findLength(s) - 1, 0));
 }
 
 /**
-   * findLength - function with 1 argument
-    * @s: char type pointer argument
-     *
-      * Description: To find length of string
-       * Return: total index count of string
-        */
+ * findLength - function with 1 argument
+ * @s: char type pointer argument
+ *
+ * Description: To find length of string
+ * Return: total index count of string
+ */
 int findLength(char *s)
 {
 if (*s != '\0')
@@ -33,14 +33,14 @@ return (0);
 }
 
 /**
-   * helper - helper function with 3 argument
-    * @s: char type pointer
-     * @len: int type argument
-      * @a: int type argument
-       *
-        * Description: to test conditions with all inputs from other functions
-	 * Return: 1 if palindrome 0 if false
-	  */
+ * helper - helper function with 3 argument
+ * @s: char type pointer
+ * @len: int type argument
+ * @a: int type argument
+ *      
+ * Description: to test conditions with all inputs from other functions
+ * Return: 1 if palindrome 0 if false
+ */
 int helper(char *s, int len, int a)
 {
 if (len ==  a)
