bca77c54f872ce6b6330c751620c501bf351b77b
Nour Hossam Eldin Radwan <nourhradwan95@gmail.com>
2022-10-12 11:00:51 -0700

    3

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+  *int_index function with 3 arguments
+  *Description: search for int
+  *@array: int type, pointer to array.
+  *@size: int type
+  *@cmp : a function pointer
+  *Return int value
+  */
+int int_index(int *array, int size, int (*cmp)(int)){
+int i;
+if(size <= 0){
+return(-1);
+}
+if(cmp && array){
+for(i = 0; i < size; i++){
+if(cmp(array[i]!= 0))
+return(i);
+else
+return(-1);
+}
+}
+}
